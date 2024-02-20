78897efc75f24e4295891772702a7c55019f6677
PongDev <pongwiwat.dev@gmail.com>
2024-02-21 01:35:35 +0700

    fix: remove print function since maltego doesn't support

--- transforms/MobileDumper.py
+++ transforms/MobileDumper.py
@@ -1,15 +1,16 @@
 from maltego_trx.entities import Person
 from maltego_trx.maltego import UIM_TYPES
 from maltego_trx.transform import DiscoverableTransform
-from maltego_trx.entities import Phrase  
+from maltego_trx.entities import Phrase
 from maltego_trx.entities import PhoneNumber
 from datetime import datetime, timedelta
 import re
 
 import sqlite3
 import os
-# from urllib.parse import unquote  
+# from urllib.parse import unquote
 # from datetime import datetime, timedelta
+
 
 class MobileDumper(DiscoverableTransform):
     @classmethod
@@ -21,48 +22,66 @@ class MobileDumper(DiscoverableTransform):
             contacts = cls.get_contacts(dataDir)
             for contact in contacts:
                 person = response.addEntity("maltego.Person", contact['name'])
-                person.addProperty(fieldName="phonenumber", displayName="Phone Number", value=contact['phone'])
+                person.addProperty(
+                    fieldName="phonenumber", displayName="Phone Number", value=contact['phone'])
                 # Use a property to indicate the category
-                person.addProperty(fieldName="category", displayName="Category", value="Contacts")
+                person.addProperty(fieldName="category",
+                                   displayName="Category", value="Contacts")
         except Exception as e:
-            response.addUIMessage("Failed to extract contacts: " + str(e), UIM_TYPES["partial"])
+            response.addUIMessage(
+                "Failed to extract contacts: " + str(e), UIM_TYPES["partial"])
 
         # Attempt to get and process SMS messages
         try:
             sms_messages = cls.get_sms(dataDir)
             for sms in sms_messages:
                 message = response.addEntity(Phrase, sms['body'])
-                message.addProperty(fieldName="from", displayName="From", value=sms['address'])
-                message.addProperty(fieldName="date", displayName="Date", value=sms['date'])
-                message.addProperty(fieldName="type", displayName="Type", value=sms['type'])
+                message.addProperty(
+                    fieldName="from", displayName="From", value=sms['address'])
+                message.addProperty(
+                    fieldName="date", displayName="Date", value=sms['date'])
+                message.addProperty(
+                    fieldName="type", displayName="Type", value=sms['type'])
                 # Use a property to indicate the category
-                message.addProperty(fieldName="category", displayName="Category", value="SMS")
+                message.addProperty(fieldName="category",
+                                    displayName="Category", value="SMS")
         except Exception as e:
-            response.addUIMessage("Failed to extract SMS: " + str(e), UIM_TYPES["partial"])
+            response.addUIMessage(
+                "Failed to extract SMS: " + str(e), UIM_TYPES["partial"])
 
         # Attempt to get and process call logs
         try:
             call_logs = cls.get_call_logs(dataDir)
             for call in call_logs:
                 call_entity = response.addEntity(PhoneNumber, call['number'])
-                call_entity.addProperty(fieldName="date", displayName="Date", value=call['date'])
-                call_entity.addProperty(fieldName="type", displayName="Type", value=call['type'])
-                call_entity.addProperty(fieldName="duration", displayName="Duration", value=str(call['duration']) + " seconds")
+                call_entity.addProperty(
+                    fieldName="date", displayName="Date", value=call['date'])
+                call_entity.addProperty(
+                    fieldName="type", displayName="Type", value=call['type'])
+                call_entity.addProperty(fieldName="duration", displayName="Duration", value=str(
+                    call['duration']) + " seconds")
                 # Use a property to indicate the category
-                call_entity.addProperty(fieldName="category", displayName="Category", value="Call Logs")
+                call_entity.addProperty(
+                    fieldName="category", displayName="Category", value="Call Logs")
         except Exception as e:
-            response.addUIMessage("Failed to extract call logs: " + str(e), UIM_TYPES["partial"])
+            response.addUIMessage(
+                "Failed to extract call logs: " + str(e), UIM_TYPES["partial"])
 
         try:
             whatsapp_messages = cls.get_whatsapp_messages(dataDir)
             for msg in whatsapp_messages:
-                message_entity = response.addEntity('maltego.Smartphone', msg['text_data'])
-                message_entity.addProperty(fieldName="timestamp", displayName="Timestamp", value=msg['timestamp'])
-                message_entity.addProperty(fieldName="key_id", displayName="Message ID", value=msg['key_id'])
+                message_entity = response.addEntity(
+                    'maltego.Smartphone', msg['text_data'])
+                message_entity.addProperty(
+                    fieldName="timestamp", displayName="Timestamp", value=msg['timestamp'])
+                message_entity.addProperty(
+                    fieldName="key_id", displayName="Message ID", value=msg['key_id'])
                 # Use a property to indicate the category
-                message_entity.addProperty(fieldName="category", displayName="Category", value="WhatsApp")
+                message_entity.addProperty(
+                    fieldName="category", displayName="Category", value="WhatsApp")
         except Exception as e:
-            response.addUIMessage("Failed to extract WhatsApp messages: " + str(e), UIM_TYPES["partial"])
+            response.addUIMessage(
+                "Failed to extract WhatsApp messages: " + str(e), UIM_TYPES["partial"])
 
         # # Get chrome history, but its not working rn idk why
         # try:
@@ -70,17 +89,17 @@ class MobileDumper(DiscoverableTransform):
         #     for history_item in browsing_history:
         #        print(history_item)
         #        response.addEntity(Phrase, 'google.com')
-              
+
         # except Exception as e:
         #     response.addUIMessage("Failed to extract browsing history: " + str(e), UIM_TYPES["partial"])
 
-
-
     # Method for get contacts
+
     @staticmethod
     def get_contacts(dataDir):
-        contacts_db = os.path.join(dataDir,"data", "com.android.providers.contacts", "databases", "contacts2.db")
-        
+        contacts_db = os.path.join(
+            dataDir, "data", "com.android.providers.contacts", "databases", "contacts2.db")
+
         contacts = []
         try:
             conn = sqlite3.connect(contacts_db)
@@ -95,17 +114,19 @@ class MobileDumper(DiscoverableTransform):
                 name, phone = row
                 contacts.append({'name': name, 'phone': phone})
         except sqlite3.Error as e:
-            print(f"Database error: {e}")
+            # print(f"Database error: {e}")
+            pass
         finally:
             if conn:
                 conn.close()
         return contacts
 
-
     # Method for get SMS
+
     @staticmethod
     def get_sms(dataDir):
-        sms_db = os.path.join(dataDir, "data", "com.android.providers.telephony", "databases", "mmssms.db")
+        sms_db = os.path.join(
+            dataDir, "data", "com.android.providers.telephony", "databases", "mmssms.db")
         sms = []
         try:
             conn = sqlite3.connect(sms_db)
@@ -116,19 +137,22 @@ class MobileDumper(DiscoverableTransform):
             for row in cursor.fetchall():
                 address, date, type, body = row
                 sms_type = "Received" if type == 1 else "Sent"
-                sms.append({'address': address, 'date': date, 'type': sms_type, 'body': body})
+                sms.append({'address': address, 'date': date,
+                           'type': sms_type, 'body': body})
         except sqlite3.Error as e:
-            print(f"Database error: {e}")
+            # print(f"Database error: {e}")
+            pass
         finally:
             if conn:
                 conn.close()
         return sms
-    
 
     # Method for get call logs
+
     @staticmethod
     def get_call_logs(dataDir):
-        calls_db = os.path.join(dataDir, "data", "com.android.providers.contacts", "databases", "calllog.db")  # Adjusted to calllog.db
+        calls_db = os.path.join(dataDir, "data", "com.android.providers.contacts",
+                                "databases", "calllog.db")  # Adjusted to calllog.db
         calls = []
         try:
             conn = sqlite3.connect(calls_db)
@@ -139,18 +163,20 @@ class MobileDumper(DiscoverableTransform):
             for row in cursor.fetchall():
                 number, date, type_code, duration = row
                 call_type = "Incoming" if type_code == 1 else "Outgoing" if type_code == 2 else "Missed"
-                calls.append({'number': number, 'date': date, 'type': call_type, 'duration': duration})
+                calls.append({'number': number, 'date': date,
+                             'type': call_type, 'duration': duration})
         except sqlite3.Error as e:
-            print(f"Database error: {e}")
+            # print(f"Database error: {e}")
+            pass
         finally:
             if conn:
                 conn.close()
         return calls
-    
 
     @staticmethod
     def get_whatsapp_messages(dataDir):
-        whatsapp_db = os.path.join(dataDir, "data", "com.whatsapp", "databases", "msgstore.db")
+        whatsapp_db = os.path.join(
+            dataDir, "data", "com.whatsapp", "databases", "msgstore.db")
         messages = []
         try:
             conn = sqlite3.connect(whatsapp_db)
@@ -169,16 +195,20 @@ class MobileDumper(DiscoverableTransform):
                                        u"\U0001FA00-\U0001FA6F"  # Chess Symbols
                                        u"\U0001FA70-\U0001FAFF"  # Symbols and Pictographs Extended-A
                                        u"\U00002702-\U000027B0"  # Dingbats
-                                       u"\U000024C2-\U0001F251" 
+                                       u"\U000024C2-\U0001F251"
                                        "]+", flags=re.UNICODE)
             for row in cursor.fetchall():
                 text_data, timestamp, key_id = row
-                text_data_clean = emoji_pattern.sub(r'', text_data)  # Remove emojis
+                text_data_clean = emoji_pattern.sub(
+                    r'', text_data)  # Remove emojis
                 timestamp_in_seconds = timestamp / 1000  # Convert from milliseconds to seconds
-                readable_time = datetime.fromtimestamp(timestamp_in_seconds).strftime('%Y-%m-%d %H:%M:%S')
-                messages.append({'text_data': text_data_clean, 'timestamp': readable_time, 'key_id': key_id})
+                readable_time = datetime.fromtimestamp(
+                    timestamp_in_seconds).strftime('%Y-%m-%d %H:%M:%S')
+                messages.append({'text_data': text_data_clean,
+                                'timestamp': readable_time, 'key_id': key_id})
         except sqlite3.Error as e:
-            print(f"Database error: {e}")
+            # print(f"Database error: {e}")
+            pass
         finally:
             if conn:
                 conn.close()
@@ -187,7 +217,8 @@ class MobileDumper(DiscoverableTransform):
         # Method for get chrome history
     @staticmethod
     def get_chrome_history(dataDir):
-        history_db = os.path.join(dataDir, "data", "com.android.chrome", "app_chrome", "Default", "History")  # Updated path
+        history_db = os.path.join(dataDir, "data", "com.android.chrome",
+                                  "app_chrome", "Default", "History")  # Updated path
         history = []
         try:
             conn = sqlite3.connect(history_db)
@@ -197,18 +228,20 @@ class MobileDumper(DiscoverableTransform):
             """)
             for row in cursor.fetchall():
                 url, title, last_visit_time = row
-                url_encoded = url.encode('utf-8', errors='ignore').decode('utf-8', errors='ignore')
-                title_encoded = title.encode('utf-8', errors='ignore').decode('utf-8', errors='ignore')
+                url_encoded = url.encode(
+                    'utf-8', errors='ignore').decode('utf-8', errors='ignore')
+                title_encoded = title.encode(
+                    'utf-8', errors='ignore').decode('utf-8', errors='ignore')
                 # Convert Chrome's timestamp (microseconds since Jan 1, 1601) to a readable datetime format
                 # Chrome's time is in microseconds; Python's datetime operates in seconds
 
-                history.append({'url': url_encoded, 'title': title_encoded, 'last_visit_time': last_visit_time})
+                history.append(
+                    {'url': url_encoded, 'title': title_encoded, 'last_visit_time': last_visit_time})
 
         except sqlite3.Error as e:
-            print(f"Database error: {e}")
+            # print(f"Database error: {e}")
+            pass
         finally:
             if conn:
                 conn.close()
         return history
-    
-
